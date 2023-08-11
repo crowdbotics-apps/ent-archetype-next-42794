125c8d45c361681db28cedf33273a76a8b7ace67
Crowdbotics <team@crowdbotics.com>
2023-08-11 17:04:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ent-workflow-canvas-42794",
+  url: "https://ent-workflow-canvas-42794.botics.co",
+  api: "https://ent-workflow-canvas-42794.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
