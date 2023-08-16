7583c1b44cf8aee149999ab533f28b9da4edb15d
Bojan Petrusevski <bojan.petrusevski@crowdbotics.com>
2023-08-16 16:04:27 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled2/index.js
@@ -0,0 +1,25 @@
+import { ImageBackground } from "react-native";
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled2 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><ImageBackground style={styles.LgfbbSAO} source={require("./download.png")} resizeMode="cover"></ImageBackground></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  LgfbbSAO: {
+    width: 1389,
+    height: 877
+  }
+});
+export default Untitled2;
