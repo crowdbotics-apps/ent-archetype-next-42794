b7e3234caf7ecf042b540f0bcd8184ca40eb0394
crowdbotics-apps <team@crowdbotics.com>
2023-08-17 07:39:26 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+class Customer(models.Model):
+    'Generated Model'
+    customer_id = models.BigIntegerField()
+    name = models.TextField(null=True,blank=True,)
+    contact_information = models.TextField(null=True,blank=True,)
+    debt = models.BigIntegerField(null=True,blank=True,)
